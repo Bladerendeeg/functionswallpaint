87a9227a78edf5fd2579a96c2b3d1dfad1f7e61a
Bladerendeeg <41634346+Bladerendeeg@users.noreply.github.com>
2021-03-23 11:32:58 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const paintWalls = function (color) {
+    console.log("The north wall has been painted " + color);
+}
+
+const wallOne = "Shiny";
+const wallTwo = "not at all";
+paintWalls(wallOne);
+paintWalls(wallTwo);
